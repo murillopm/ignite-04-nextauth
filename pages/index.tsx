98af9c08f57e92277f24532d961ba2d5c9b8fd32
Murillo Mesquita <mu.pracucio@gmail.com>
2021-08-05 18:33:16 -0300

    Validando visitante

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import { GetServerSideProps } from "next"
 import { FormEvent, useContext, useState } from "react"
-import { parseCookies } from 'nookies'
 
 import { AuthContext } from "../contexts/AuthContext"
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from "../utils/withSSRGuest"
 
 export default function Home() {
   const [email, setEmail] = useState('')
@@ -29,19 +29,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
-
-  if(cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
-    }
-  }
-  
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-}
+})
